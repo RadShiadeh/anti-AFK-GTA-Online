71cd73ec47b23b8b4b3b2704c4229d96d7199f3e
Radmehr Shiadeh <78790490+Glonki4@users.noreply.github.com>
2024-03-11 17:28:01 +0000

    fixed bug, not the right key being pressed, added mouse move

--- bot_move.cpp
+++ bot_move.cpp
@@ -58,41 +58,22 @@ void ReleaseKey(USHORT hexKeyCode) {
     SendInput(1, lpInput, sizeof(INPUT));
 }
 
-void randMove(USHORT keyCodeA, USHORT keyCodeS, USHORT keyCodeD, USHORT keyCodeW) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+void wiggleMouse(LONG dx, LONG dy) {
+    Input_I ii;
+    ii.mi = {dx, dy, 0, MOUSEEVENTF_MOVE, 0, 0};
+    Input x = {INPUT_MOUSE, ii};
 
-    double mean = 4.0;
-    double std = 2.0;
-
-    std::normal_distribution<double> norm(mean, std);
-    double r = norm(gen);
-
-    int randint = int(r) % 5;
-
-    if (randint == 0) {
-        PressKey(keyCodeA);
-    } else if (randint == 1) {
-        PressKey(keyCodeS);
-    } else if (randint == 2) {
-        PressKey(keyCodeD);
-    } else {
-        PressKey(keyCodeW);
-    }
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    ReleaseKey(keyCodeA);
-    ReleaseKey(keyCodeS);
-    ReleaseKey(keyCodeD);
-    ReleaseKey(keyCodeW);
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    LPINPUT lpInput = reinterpret_cast<LPINPUT>(&x);
+    SendInput(1, lpInput, sizeof(INPUT));
 }
 
 int main() {
     while (true) {
-        randMove(0x41, 0x53, 0x44, 0x11);
+        PressKey(0x11);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        ReleaseKey(0x11);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        wiggleMouse(101, 110);
     }
 
     return 0;
